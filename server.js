41ba937d29884c583925b5bf36b935d34824d400
Carmen Regina <crcarmen23@gmail.com>
2023-10-19 15:13:17 -0500

    added starting GET routes

--- server.js
+++ server.js
@@ -11,4 +11,17 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+  fs.readFile(path.join(__dirname, './db/db.json'))
+  .then
+);
+
+
+
+
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
