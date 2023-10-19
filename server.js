1ef80b8e0d21a9ab18895722b110c46345ab9e92
Carmen Regina <crcarmen23@gmail.com>
2023-10-19 17:54:20 -0500

    added code in server.js to get notes to save

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -17,22 +20,20 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => {
-    var data = fs.readFileSync(path.join(__dirname, '/db/db.json'))
+    var data = fs.readFileSync(path.join(__dirname, './db/db.json'))
     res.send(JSON.parse(data))
 }
 );
 
-app.post('/api/notes', ({body}, res) => {
+app.post('/api/notes', (req, res) => {
     var data = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json')))
-    data.push(body)
-    console.log(body)
-
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'),JSON.stringify(data))
-
-    res.send(JSON.stringify(data))
+    let noteText = req.body;
+    let uuid = data.length;
+    noteText.id = uuid + 1;
+    data.push(noteText);
+    fs.writeFileSync("./db/db.json", JSON.stringify(data))
+    res.json(data)
 })
-
-
 
 
 
