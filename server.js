d817d6205de2bea395675303a097261d13046bb9
Carmen Regina <crcarmen23@gmail.com>
2023-10-19 15:51:34 -0500

    added server.js code

--- server.js
+++ server.js
@@ -2,23 +2,35 @@ const express = require('express');
 const path = require('path');
 const termData = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs')
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-  fs.readFile(path.join(__dirname, './db/db.json'))
-  .then
+app.get('/api/notes', (req, res) => {
+    var data = fs.readFileSync(path.join(__dirname, '/db/db.json'))
+    res.send(JSON.parse(data))
+}
 );
+
+app.post('/api/notes', ({body}, res) => {
+    var data = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json')))
+    data.push(body)
+    console.log(body)
+
+    fs.writeFileSync(path.join(__dirname, '/db/db.json'),JSON.stringify(data))
+
+    res.send(JSON.stringify(data))
+})
 
 
 
